2aac01583f7b9a18b7b333b4cb66a2423999ef95
furuoshan <108115675+furuoshan@users.noreply.github.com>
2023-04-14 18:25:36 +0800

    Update shudu.py

--- 小实验1/shudu.py
+++ 小实验1/shudu.py
@@ -1,5 +1,7 @@
 # 数独
-def isvalid(arr:list,k:int,row:int,col:int):
+import numpy as np
+
+def is_valid(k: int,row: int,col: int):
     for i in range(9):
         if arr[row][i]==k:
             return False
@@ -12,37 +14,33 @@ def isvalid(arr:list,k:int,row:int,col:int):
             if k==l:
                 return False
     return True
-def isempty(arr):
+def is_empty():
     for i in range(9):
         for j in range(9):
             if arr[i][j]==0:
                 return True,i,j
     return False,9,9
-def shudu(arr:list):
-    a=isempty(arr)
+def sudoku():
+    a=is_empty()
     if not a[0]:
         return True
     else:
-        i=a[1]
-        j=a[2]
+        i = a[1]
+        j = a[2]
     for k in range(1, 10):
-        if isvalid(arr, k, i, j):
+        if is_valid(k, i, j):
             arr[i][j] = k
-            if shudu(arr):
+            if sudoku():
                 return True
             arr[i][j] = 0
     return False
 
+
 if __name__ == '__main__':
-    arr = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-           [6, 0, 0, 1, 9, 5, 0, 0, 0],
-           [0, 9, 8, 0, 0, 0, 0, 6, 0],
-           [8, 0, 0, 0, 6, 0, 0, 0, 3],
-           [4, 0, 0, 8, 0, 3, 0, 0, 1],
-           [7, 0, 0, 0, 2, 0, 0, 0, 6],
-           [0, 6, 0, 0, 0, 0, 2, 8, 0],
-           [0, 0, 0, 4, 1, 9, 0, 0, 5],
-           [0, 0, 0, 0, 8, 0, 0, 7, 9]]
-    shudu(arr)
-    for i in arr:
-        print(i)
+    arr = np.arange(81).reshape(9, 9)
+    print("please input 9 lines and each line has 9 numbers")
+    for p in range(9):
+        for q, m in enumerate(list(map(int, input().split()))):
+            arr[p][q] = m
+    sudoku()
+    print("The answer of the sudoku is:\n", arr)
